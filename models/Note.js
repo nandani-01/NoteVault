7ac380a43f4088ce96c89e6986950fc487c2ce89
nandani-01 <125036796+nandani-01@users.noreply.github.com>
2024-06-09 15:16:11 +0530

    addign models files

---
+++ models/Note.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const noteSchema = new mongoose.Schema({
+    email: {type: String , required: true},
+    title: {type: String , required: true},
+    desc :{type: String , required: true},
+},{timestamps: true});
+
+module.exports = mongoose.model('Note' , noteSchema)
